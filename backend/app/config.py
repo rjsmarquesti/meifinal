894f4d21fd5542a27bb7feb8d5ea164e0b548a83
Rogério Marques <rjsmarques.ti@gmail.com>
2026-02-21 01:46:27 -0300

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,20 @@
-# Configurações
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://meifiscal:An@Beatriz270172@meifiscal-db:5432/meifiscal")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
