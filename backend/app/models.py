39aa95f69c4e842028491ac540b2a40c016cbcf9
Rogério Marques <rjsmarques.ti@gmail.com>
2026-02-21 16:11:56 -0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
 class Cliente(Base):
@@ -9,3 +10,15 @@ class Cliente(Base):
     email = Column(String, nullable=True, index=True)
     cpf = Column(String, nullable=True, index=True)
     telefone = Column(String, nullable=True)
+
+    notas = relationship("Nota", back_populates="cliente")
+
+class Nota(Base):
+    __tablename__ = "notas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    cliente_id = Column(Integer, ForeignKey("clientes.id"), nullable=False)
+    valor = Column(Float, nullable=False)
+    descricao = Column(String, nullable=True)
+
+    cliente = relationship("Cliente", back_populates="notas")
