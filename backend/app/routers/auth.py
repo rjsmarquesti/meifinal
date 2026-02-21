136c59bf504751a8364fc86bbfe07ac8f405fe75
Rogério Marques <rjsmarques.ti@gmail.com>
2026-02-21 15:02:59 -0300

    Update auth.py

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,1 +1,11 @@
 # Rotas Auth
+from fastapi import APIRouter
+
+# Esta é a variável que o main.py está procurando!
+router = APIRouter()
+
+@router.get("/test-auth")
+def test_auth():
+    return {"ok": True, "msg": "O roteador de autenticação foi encontrado!"}
+
+# Depois colocaremos aqui as rotas de Login e Cadastro reais
